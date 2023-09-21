03d57a4a7e7276e7da468fc0837dbb03c2625626
AllyD <Ally@ileader.com>
2023-09-21 15:37:45 -0400

    added position class and unti test

---
+++ LevelUpGame.Tests/levelup/PositionTest.cs
@@ -0,0 +1,20 @@
+﻿using NUnit.Framework;
+using levelup;
+using LevelUpGame.levelup;
+
+namespace LevelUpGame.Test.levelup
+{
+    [TestFixture]
+    
+    public class PositionTest
+    {
+        [Test]
+        public void Position()
+        {
+            var positionObj = new Position(0, 0);
+
+            Assert.AreEqual(0, positionObj.coordinates.xCoordinates);
+            Assert.AreEqual(0, positionObj.coordinates.yCoordinates);
+        }
+    }
+}
---
+++ LevelUpGame/levelup/Position.cs
@@ -0,0 +1,13 @@
+﻿namespace LevelUpGame.levelup
+{
+    public class Position
+    {
+        public (int xCoordinates, int yCoordinates) coordinates;
+
+        public Position(int xCoordinates, int yCoordinates)
+        {
+            coordinates.xCoordinates = xCoordinates;
+            coordinates.yCoordinates = yCoordinates;
+        }   
+    }
+}
