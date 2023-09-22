b682207e61eedabbad8b815090b0861d063aa8fb
make bootstrap <michele.fisher33@gmail.com>
2023-09-22 15:09:50 +0000

    update gamecontroller test

--- LevelUpGame.Tests/levelup/GameControllerTest.cs
+++ LevelUpGame.Tests/levelup/GameControllerTest.cs
@@ -20,5 +20,29 @@ namespace levelup
 #pragma warning disable CS8602 // Rethrow to preserve stack details
             Assert.IsNotNull(testObj.GetStatus());
         }
+        [Test]
+        public void test_move()
+        {
+        }
+        [Test]
+        public void test_createCharacter()   
+        {
+        }
+        [Test]
+        public void test_startGame()
+        {
+
+        }
+        [Test]
+        public void test_getStatus()
+        {
+
+        }
+        [Test]
+        public void test_setCharacterPosition()
+        {
+
+        }
+       
     }
 }
